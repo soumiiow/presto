cbb27489c57fb8227efc91816e67b34c8311f2de
Soumya Duriseti <soumyaduriseti@gmail.com>
2025-06-06 12:22:09 -0700

    Add custom function native tests
    Co-authored-by: Pramod <pramod.satya@ibm.com>

--- presto-tests/src/main/java/com/facebook/presto/tests/AbstractTestQueries.java
+++ presto-tests/src/main/java/com/facebook/presto/tests/AbstractTestQueries.java
@@ -4589,37 +4589,6 @@ public abstract class AbstractTestQueries
     }
 
     @Test
-    public void testCustomAdd()
-    {
-        assertQuery(
-                "SELECT custom_add(orderkey, custkey) FROM orders",
-                "SELECT orderkey + custkey FROM orders");
-    }
-
-    @Test
-    public void testCustomSum()
-    {
-        @Language("SQL") String sql = "SELECT orderstatus, custom_sum(orderkey) FROM orders GROUP BY orderstatus";
-        assertQuery(sql, sql.replace("custom_sum", "sum"));
-    }
-
-    @Test
-    public void testCustomRank()
-    {
-        @Language("SQL") String sql = "" +
-                "SELECT orderstatus, clerk, sales\n" +
-                ", custom_rank() OVER (PARTITION BY orderstatus ORDER BY sales DESC) rnk\n" +
-                "FROM (\n" +
-                "  SELECT orderstatus, clerk, sum(totalprice) sales\n" +
-                "  FROM orders\n" +
-                "  GROUP BY orderstatus, clerk\n" +
-                ")\n" +
-                "ORDER BY orderstatus, clerk";
-
-        assertEquals(computeActual(sql), computeActual(sql.replace("custom_rank", "rank")));
-    }
-
-    @Test
     public void testApproxSetBigint()
     {
         MaterializedResult actual = computeActual("SELECT cardinality(approx_set(custkey)) FROM orders");
